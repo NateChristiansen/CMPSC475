36e7ee7b7d076645a89589a8ce557edefc292367
Nate Christiansen <natechristiansen42@gmail.com>
2017-01-18 22:36:13 -0500

    finished w2c1

---
+++ W2C1/app/src/main/java/cmpsc475/w2c1/W2C2.java
@@ -0,0 +1,13 @@
+package cmpsc475.w2c1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class W2C2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w2_c2);
+    }
+}
