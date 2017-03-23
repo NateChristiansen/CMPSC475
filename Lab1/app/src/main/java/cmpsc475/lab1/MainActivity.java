4335115fb03d186f4304c8349dfc0c68280a2f17
Nate Christiansen <natechristiansen42@gmail.com>
2017-03-23 18:45:42 -0400

    finished lab1

--- Lab1/app/src/main/java/cmpsc475/lab1/MainActivity.java
+++ Lab1/app/src/main/java/cmpsc475/lab1/MainActivity.java
@@ -14,24 +14,25 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String[] words = {
+                "Project 1",
                 "What is the greatest video game series of all time?",
                 "What are the 2 best games in this series?",
                 "Who is the best boss in this series?", // one of 6 choices, calls new activity that sends selection and either says what is wrong or shows correct, keep track of tries, 3 tries or fail
                 "Question 4",
                 "Question 5"
         };
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, R.id.desc, words));
+        setListAdapter(new ArrayAdapter<>(this, R.layout.activity_main, R.id.desc, words));
     }
 
     protected void onListItemClick(ListView l, View v, int position, long id) {
         switch (position) {
-            case 0:
+            case 1:
                 startActivity(new Intent(this, Question.class));
                 break;
-            case 1:
+            case 2:
                 startActivity(new Intent(this, MoreInfo.class));
                 break;
-            case 2:
+            case 3:
                 startActivity(new Intent(this, BestBoss.class));
         }
     }
--- Lab1/app/src/main/java/cmpsc475/lab1/MoreInfo.java
+++ Lab1/app/src/main/java/cmpsc475/lab1/MoreInfo.java
@@ -15,13 +15,21 @@ public class MoreInfo extends AppCompatActivity {
     }
 
     public void bloodbornewiki(View view) {
-        overridePendingTransition(R.animator.fadein, R.animator.fadeout);
+        //overridePendingTransition(R.animator.fadein, R.animator.fadeout);
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Bloodborne")));
+        overridePendingTransition(R.animator.fadeout, R.animator.fadein);
     }
 
     public void darksoulswiki(View view) {
-        overridePendingTransition(R.animator.fadein, R.animator.fadeout);
+        //
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Dark_Souls")));
+        overridePendingTransition(R.animator.fadeout, R.animator.fadein);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //overridePendingTransition(R.animator.fadein, R.animator.fadeout);
     }
 
     public void end(View view) {
